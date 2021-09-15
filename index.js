641a9612618936f39733acf9b3e2408680c3f6d0
Dipshikha Shrestha <shresthadipshikha86@gmail.com>
2021-09-15 08:40:10 +0545

    Study of JS datatype

---
+++ index.js
@@ -0,0 +1,55 @@
+//object dheari value store garna /mulitple classes insert garnalai
+//array
+//number
+//string
+//boolean
+//float
+//datetime
+//null
+//underfined
+
+let a = 1; //number
+let b = "Hello world"; //string
+let c = true; //booleam
+let d = 1.5; //float
+
+console.log(a);
+
+a = "IIMS"
+
+console.log(a);
+
+const example = "Hello world";
+
+console.log(example);
+
+//example = "IIMS"; this is error const varable ma re assign garna mildaina
+//console.log(example);
+
+let iims = null;
+console.log(iims);
+
+
+//object banauni ani value rakhni 
+let person = {
+   name: 'Dipshikha',
+   age: 20,
+   address: "Kathmandu",
+   phone: 9880162286,
+   location:{
+       lattitude: 76478,
+       longitude: 637783
+   }
+};
+console.log("This person name is:", person.name);
+console.log("This person address is:", person.address);
+console.log("This person location is:", person.location);
+
+let exampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//let exampleArray2 = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//another way of adding variables
+console.log(exampleArray);
+//console.log(exampleArray2);
+
+let studentNames = ["Dip", "Ram", "shina"];
+console.log(studentNames)
