b55c77d8f4dd207778b4e48dcd423d7cb78adce1
Dipshikha Shrestha <shresthadipshikha86@gmail.com>
2021-09-19 08:33:10 +0545

    adding examples of operators

---
+++ callback-example.js
@@ -0,0 +1,41 @@
+// const makeStringUpperCase = (string) => {
+//     return string.toUpperCase();
+// };
+// const makeStringLowerCase = (string) => {
+//     return string.toLowerCase();
+// };
+
+// // console.log(makeStringUpperCase("Hello", makeUpperString));
+// // console.log(makeStringLowerCase("Hello", makeLowerString));
+
+// const passUserInput = (input, callback) => {
+//  input = "Hello" + input;
+//  return callback(input);
+// };
+
+// console.log(passUserInput("IIMS", makeStringLowerCase))
+// console.log(passUserInput("IIMS", makeStringUpperCase))
+
+// //one line function for callback
+// const add = (a , b) => a + b;
+// const subtract = (a , b) => a - b;
+// const multiply = (a , b) => a * b;
+
+// const callBackExample =(a , b , mathCallBack) => {
+//     console.log(mathCallBack(a , b));
+// }
+// //function le function pathyo ani unction callback garyo
+// callBackExample(2 , 2, add);
+// callBackExample(2 , 2, multiply);
+// callBackExample(2 , 2, subtract);
+
+// //"" ma rakhda string ma "" sign nahuda chai concat ma jancha ani 12 print huncha
+// let a = "1";
+// let b = "2";
+// //console.log(a + b);
+// console.log(parseInt(a) + parseInt(b));
+// //parseInt changes into number ani add huncha 
+
+let a = 2;
+a = a + a; // a -=a;
+console.log(a);
---
+++ operators.js
@@ -0,0 +1,23 @@
+let a = 2;
+let b = 1
+
+a += b;
+
+// if (1 == 1){ // value matra check garcha == le datatype mistake vayeani
+//     console.log("this statement is true");
+// }
+
+// if (2 === "2"){ //strictly value ra datatype check gracha 3ota === le
+//     console.log("this statement is true");
+// }
+
+// if (1 === "1"){ 
+//     console.log("this statement is true");
+// }
+// else{
+//     console.log("this statement is false");
+// }
+
+console.log(1 == 9 ? "this statement is true" : "this statement is false");
+//true ho vani ? pachi ko print huncha 
+//false ho vani : pachiko print huncha
